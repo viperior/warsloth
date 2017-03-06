cc62c48b0465e6ca90cdda8d0afa63a379426f38
Jay Gossett <jay.d.gossett+github@gmail.com>
2017-03-06 14:29:53 -0600

    Adjusted upload target directory

--- public/getfile.php
+++ public/getfile.php
@@ -2,7 +2,7 @@
 <br>
 
 <?php
-$target_dir = "../";
+$target_dir = "./";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
