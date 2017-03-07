e366a9868d8c005cc9171b13920e19093bfdce46
Luke Stone <viperior@gmail.com>
2017-03-07 04:47:58 -0600

    Enhance user feedback on pin entry and change pin

--- public/js/pin.js
+++ public/js/pin.js
@@ -2,7 +2,7 @@ function check_pin() {
   var pin_input_element = document.getElementById("pin-code-input");
   var pin_entered = parseInt(pin_input_element.value);
   var pin_feedback = document.getElementById("pin-code-feedback");
-  var correct_pin = 1234;
+  var correct_pin = 1970;
   var is_correct_pin = pin_entered == correct_pin;
 
   if( is_correct_pin )
@@ -12,7 +12,13 @@ function check_pin() {
   }
   else
   {
-    pin_feedback.innerHTML = "Invalid PIN. Please try again.";
+    if( isNaN(pin_entered) )
+    {
+      pin_feedback.innerHTML = "You must enter a 4-digit pin to login. Please try again.";
+    }
+    else {
+      pin_feedback.innerHTML = "Invalid PIN Entered: " + String(pin_entered) + ". Please try again.";
+    }
   }
 }
 
