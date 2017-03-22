317a3ce1f14de4cd71374ee542c4c3d0626510e7
Jay Gossett <jay.d.gossett+github@gmail.com>
2017-03-22 17:37:50 -0500

    Added styling classes to form
    Added bootstrap classes to the upload page so its form will have the
    same style as the other forms on the site.

--- public/upload.php
+++ public/upload.php
@@ -1,14 +1,20 @@
-<?php include "includes/header.php"; ?>
 <!DOCTYPE html>
+<?php include "includes/header.php"; ?>
+
+<hr>
+
 <div class="row">
 	<div class="col-sm-12">
 		<h2>Upload File</h2>
-    <form action="getfile.php" method="post" enctype="multipart/form-data">
-        Select file to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <br>
-        <input type="submit" value="Upload File" name="submit">
-    </form>
+		
+		<form action="getfile.php" method="post" enctype="multipart/form-data">
+			<div class="form-group">
+				<label>Select file to upload: 
+				<input class="form-control" type="file" name="fileToUpload" id="fileToUpload" />
+				</label>
+				<button class="btn btn-primary" type="submit">Upload File</button>
+			</div>
+		</form>
   </div>
 </div>
 <?php include "includes/footer.php"; ?>
