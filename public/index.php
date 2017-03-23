c9012ce8615bcaa60faaffb32ac78e2df09e1276
Luke Adair <j.luke.adair@gmail.com>
2017-03-22 19:55:23 -0500

    style changes

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@
 		<p>Introduce the visitor to the business using clear, informative text. Use well-targeted keywords within your sentences to make sure search engines can find the business.</p>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et molestiae similique eligendi reiciendis sunt distinctio odit? Quia, neque, ipsa, adipisci quisquam ullam deserunt accusantium illo iste exercitationem nemo voluptates asperiores.</p>
 		<p>
-			<a class="btn btn-default btn-lg" href="#">Hire Cobra &raquo;</a>
+			<a class="btn btn-default btn-lg" href="contact.php">Hire Cobra &raquo;</a>
 		</p>
 	</div>
 </div>
@@ -19,17 +19,23 @@
 
 <div class="row">
 	<div class="col-sm-4">
-		<img class="img-circle img-responsive img-center" src="img/arms-sales.png" alt="">
+		<div class="circle" style="background-color: red;">
+			<img class="img-circle img-responsive img-center" src="img/arms-sales.png" alt="">
+		</div>
 		<h2>Arms Sales</h2>
 		<p>There are over 550 million firearms in worldwide circulation. That's one firearm for every twelve people on the planet. The only question is: How do we arm the other 11?</p>
 	</div>
 	<div class="col-sm-4">
-		<img class="img-circle img-responsive img-center" src="http://placehold.it/300x300" alt="">
+		<div class="circle" style="background-color: red;">
+			<img class="img-circle img-responsive img-center" src="img/pyramid.png" alt="">
+		</div>
 		<h2>Multi-Level Marketing</h2>
 		<p>All businesses are pyramidal in structure, but some are more pyramidal than others. We connect hard-working down-line distributors with generous overlords who help them help us.</p>
 	</div>
 	<div class="col-sm-4">
-		<img class="img-circle img-responsive img-center" src="http://placehold.it/300x300" alt="">
+		<div class="circle" style="background-color: red;">
+			<img class="img-circle img-responsive img-center" src="http://placehold.it/300x300" alt="">
+		</div>
 		<h2>Semi-Legimate Business Schemes</h2>
 		<p>I say, "potato", you say "federal felony." Cobra takes pride in braving the greyer areas of entreprise.</p>
 	</div>
