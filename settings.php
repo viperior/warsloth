f8ca91bb3e988544cdd555a8dc2e0cc826112642
Luke Adair <j.luke.adair@gmail.com>
2017-04-10 19:49:04 -0500

    added settings for all teams

--- settings.php
+++ settings.php
@@ -1,7 +1,43 @@
 <?php 
+	// change this to be the desired team
+	$team = "footclan";
 
-	$companyName = "Cobra";
-	$companyBodyClass = "cobra";
-	$ceoEmail = "ceo@cobra.com";
+	$settings = array(
+		'cobra' => array(),
+		'motherbrain' => array(),
+		'redfalcon' => array(),
+		'footclan' => array(),
+		'decepticons' => array()
+	);
+
+	// Cobra
+	$settings['cobra']['companyName'] = "Cobra";
+	$settings['cobra']['companyBodyClass'] = "cobra";
+	$settings['cobra']['ceoEmail'] = "ceo@cobra.com";
+
+	// Motherbrain
+	$settings['motherbrain']['companyName'] = "Motherbrain";
+	$settings['motherbrain']['companyBodyClass'] = "motherbrain";
+	$settings['motherbrain']['ceoEmail'] = "ceo@motherbrain.com";
+
+	// Red Falcon
+	$settings['redfalcon']['companyName'] = "Red Falcon";
+	$settings['redfalcon']['companyBodyClass'] = "redfalcon";
+	$settings['redfalcon']['ceoEmail'] = "ceo@redfalcon.com";
+
+	// Foot Clan
+	$settings['footclan']['companyName'] = "Foot Clan";
+	$settings['footclan']['companyBodyClass'] = "footclan";
+	$settings['footclan']['ceoEmail'] = "ceo@footclan.com";
+
+	// Decepticons
+	$settings['decepticons']['companyName'] = "Decepticons";
+	$settings['decepticons']['companyBodyClass'] = "decepticons";
+	$settings['decepticons']['ceoEmail'] = "ceo@decepticons.com";
+
+	// init settings
+	$companyName = $settings[$team]['companyName'];
+	$companyBodyClass = $settings[$team]['companyBodyClass'];
+	$ceoEmail = $settings[$team]['ceoEmail'];
 
  ?>
