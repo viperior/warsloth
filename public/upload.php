526adbb45ae8a2f75be2386fa0d84969be33463a
Luke Stone <viperior@gmail.com>
2017-04-20 15:07:09 -0500

    Require user login to use file upload feature

--- public/upload.php
+++ public/upload.php
@@ -6,25 +6,35 @@
 <div class="row">
 	<div class="col-sm-6">
 		<h2>Upload File</h2>
-		
-		<form action="getfile.php" method="post" enctype="multipart/form-data">
-			<div class="form-group">
-				<div class="row">
-					<div class="col-sm-12" id="upload-background">
-						<div class="selectFile btn btn-primary">
-							<label>Select File 
-							<input class="form-control upload" type="file" name="fileToUpload" id="fileToUpload"/>
-							</label>
+
+		<?php
+			if(isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] == true)
+			{
+				echo '<form action="getfile.php" method="post" enctype="multipart/form-data">
+					<div class="form-group">
+						<div class="row">
+							<div class="col-sm-12" id="upload-background">
+								<div class="selectFile btn btn-primary">
+									<label>Select File
+									<input class="form-control upload" type="file" name="fileToUpload" id="fileToUpload"/>
+									</label>
+								</div>
+								<input id="displayFile" placeholder="Select file" disabled="disabled" />
+							</div>
+
+							<div class="col-sm-12">
+								<button class="btn btn-primary" type="submit" id="uploadSubmit">Upload</button>
+							</div>
 						</div>
-						<input id="displayFile" placeholder="Select file" disabled="disabled" />
 					</div>
-					
-					<div class="col-sm-12">
-						<button class="btn btn-primary" type="submit" id="uploadSubmit">Upload</button>
-					</div>
-				</div>
-			</div>
-		</form>
+				</form>';
+			}
+			else
+			{
+				echo 'Please <a href="login.php">log in</a> to upload a file.';
+			}
+		?>
+
   </div>
 </div>
 
